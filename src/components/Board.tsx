17636f0d70d137328004a085f2381697ff85e0f1
micccAngelo <michaelangelo0109@gmail.com>
2025-03-20 18:29:04 +0700

    Add Board and Cell component

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,5 +1,32 @@
-const Board = () => {
-  return <div>Board</div>;
+import React from "react";
+import Cell from "./Cell";
+import "../styles/board.css";
+
+type BoardProps = {
+  value: string | null;
+  onCellClick: () => void;
+};
+
+const Board: React.FC<BoardProps> = ({ value, onCellClick }) => {
+  return (
+    <div className="board">
+      <div className="board-row">
+        <Cell value={value} onCellClick={onCellClick} />
+        <Cell value={value} onCellClick={onCellClick} />
+        <Cell value={value} onCellClick={onCellClick} />
+      </div>
+      <div className="board-row">
+        <Cell value={value} onCellClick={onCellClick} />
+        <Cell value={value} onCellClick={onCellClick} />
+        <Cell value={value} onCellClick={onCellClick} />
+      </div>
+      <div className="board-row">
+        <Cell value={value} onCellClick={onCellClick} />
+        <Cell value={value} onCellClick={onCellClick} />
+        <Cell value={value} onCellClick={onCellClick} />
+      </div>
+    </div>
+  );
 };
 
 export default Board;
--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,5 +1,17 @@
-const Cell = () => {
-  return <div>Cell</div>;
+import React from "react";
+import "../styles/board.css";
+
+type CellProps = {
+  value: string | null;
+  onCellClick: () => void;
+};
+
+const Cell: React.FC<CellProps> = ({ value, onCellClick }) => {
+  return (
+    <button className="cell" onClick={onCellClick}>
+      {value}
+    </button>
+  );
 };
 
 export default Cell;
