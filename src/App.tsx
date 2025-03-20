7fcd1f6c7002263bfa4b6e07a7ce878adf045284
micccAngelo <michaelangelo0109@gmail.com>
2025-03-20 18:41:02 +0700

    Fix Board Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./styles/index.css";
 import HomePage from "./pages/HomePage";
+import TicTacToe from "./pages/TicTacToe";
 
 function App() {
   return (
     <>
       <HomePage />
+      <TicTacToe />
     </>
   );
 }
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,27 +3,27 @@ import Cell from "./Cell";
 import "../styles/board.css";
 
 type BoardProps = {
-  value: string | null;
-  onCellClick: () => void;
+  squares: (string | null)[];
+  onCellClick: (index: number) => void;
 };
 
-const Board: React.FC<BoardProps> = ({ value, onCellClick }) => {
+const Board: React.FC<BoardProps> = ({ squares, onCellClick }) => {
   return (
     <div className="board">
       <div className="board-row">
-        <Cell value={value} onCellClick={onCellClick} />
-        <Cell value={value} onCellClick={onCellClick} />
-        <Cell value={value} onCellClick={onCellClick} />
+        <Cell value={squares[0]} onCellClick={() => onCellClick(0)} />
+        <Cell value={squares[1]} onCellClick={() => onCellClick(1)} />
+        <Cell value={squares[2]} onCellClick={() => onCellClick(2)} />
       </div>
       <div className="board-row">
-        <Cell value={value} onCellClick={onCellClick} />
-        <Cell value={value} onCellClick={onCellClick} />
-        <Cell value={value} onCellClick={onCellClick} />
+        <Cell value={squares[3]} onCellClick={() => onCellClick(3)} />
+        <Cell value={squares[4]} onCellClick={() => onCellClick(4)} />
+        <Cell value={squares[5]} onCellClick={() => onCellClick(5)} />
       </div>
       <div className="board-row">
-        <Cell value={value} onCellClick={onCellClick} />
-        <Cell value={value} onCellClick={onCellClick} />
-        <Cell value={value} onCellClick={onCellClick} />
+        <Cell value={squares[6]} onCellClick={() => onCellClick(6)} />
+        <Cell value={squares[7]} onCellClick={() => onCellClick(7)} />
+        <Cell value={squares[8]} onCellClick={() => onCellClick(8)} />
       </div>
     </div>
   );
