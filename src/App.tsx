df5bdb5d4e4a1db0fa1992fe15b2e95ca331540d
DanielJonthn <daniel.378.dj@gmail.com>
2025-03-20 18:44:19 +0700

    Add HomePage + Relocate Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import "./styles/index.css";
+import "./styles/App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import TicTacToe from "./pages/TicTacToe";
+import TicTacToe2 from "./pages/TicTacToe2";
 
 function App() {
   return (
     <>
-      <HomePage />
+      <BrowserRouter>
+        <div className="app">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/classic" element={<TicTacToe />} />
+            <Route path="/enhanced" element={<TicTacToe2 />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
